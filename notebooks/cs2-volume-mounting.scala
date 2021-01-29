1a12ee35fec5a2c631fe7f37a113bd92d4fa45c7
Nishtha Goswami <35414417+Nishtha9@users.noreply.github.com>
2021-01-29 22:10:29 +0530

    Update: README.md notebooks

--- notebooks/cs2-volume-mounting.scala
+++ notebooks/cs2-volume-mounting.scala
@@ -2,25 +2,44 @@
 val configs = Map(
   "fs.azure.account.auth.type" -> "OAuth",
   "fs.azure.account.oauth.provider.type" -> "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
-  "fs.azure.account.oauth2.client.id" -> "f84616aa-d6be-478e-b2fc-9ef7c9ca078b",
+  "fs.azure.account.oauth2.client.id" -> "e4edd8c1-2026-4e42-a8bf-bdbcc02df168",
   "fs.azure.account.oauth2.client.secret" -> dbutils.secrets.get(scope = "training-scope", key = "appsecret"),
-  "fs.azure.account.oauth2.client.endpoint" -> "https://login.microsoftonline.com/381a10df-8e85-43db-86e1-8893b075b027/oauth2/token")
+  "fs.azure.account.oauth2.client.endpoint" -> "https://login.microsoftonline.com/f162b8ca-3a8c-4d22-a509-3a0f10dd3695/oauth2/token")
 
 // COMMAND ----------
 
-val mounts=dbutils.fs.mounts()
-val mountPath="/mnt/data"
-val isExist=false
+import scala.util.control._
 
-for (mount <- mounts){
-  if (mount.mountPoint == mountPath){
-    isExist = true
-    break
+// COMMAND ----------
+
+var mounts=dbutils.fs.mounts()
+var mountPath="/mnt/data"
+var isExist=false
+
+val outer = new Breaks;
+
+outer.breakable {
+  for(mount <- mounts) {
+    if(mount.mountPoint == mountPath) {
+      isExist = true;
+      outer.break;
+    }
   }
 }
 
 
+// COMMAND ----------
+
+if(isExist) {
+  println("Volume Mounting for Case Study Data Already Exist!")
+}
+else {
+  dbutils.fs.mount(
+    source = "abfss://casestudydata@iomegadls.dfs.core.windows.net/",
+    mountPoint = "/mnt/data",
+    extraConfigs = configs)
+}
+
 
 // COMMAND ----------
 
-println("Exist status "+ isExist)
